5f95c45098439f2e69a680dc5af839b210f9cadf
waelHP <wael.hassan.mec@gmail.com>
2024-08-04 20:20:42 +0200

    remove all files

--- Embedded_C/Unit2/LAB/startup.s
+++
@@ -1,5 +0,0 @@
-.globl reset
-reset:
-    ldr sp, =0x00011000
-    bl main
-stop: b stop
--- Embedded_C/Unit2/LAB/uart.h
+++
@@ -1,9 +0,0 @@
-#ifndef _UART_H
-#define _UART_H
-
-
-
-void Uart_send_string(unsigned char* P_tx_string);
-
-
-#endif
